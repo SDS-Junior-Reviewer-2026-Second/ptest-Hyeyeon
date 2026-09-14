edd2a0c1f50e68c3fbdffff9e082a3ecef366878
Hey0140 <93310801+Hey0140@users.noreply.github.com>
2026-09-14 14:14:47 +0900

    Implement basic calculator operations

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+
+public class Calculator {
+    public int result = 0;
+
+    public void plus(int a, int b) {
+        result = a + b;
+    }
+
+    public void minus(int a, int b) {
+        result = a - b;
+    }
+
+    public void divide(int a, int b) {
+        if (b == 0) {
+            result = 0;
+            System.out.println("0으로 나눌 수 없습니다");
+        } else {
+            result = a / b;
+        }
+    }
+
+    public void multiple(int a, int b) {
+        result = a * b;
+    }
+
+    public void printResult() {
+        System.out.println(result);
+    }
+}
