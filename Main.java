62f381b78c72a9e3fcd11594d649095c02f00d7a
Hey0140 <lehy1124@soongsil.ac.kr>
2026-09-14 15:41:52 +0900

    [FIX] main error fix

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SuperCar car = new SuperCar("붕붕이");
-        car.ride();
+        // SuperCar car = new SuperCar("붕붕이");
+        // car.ride();
     }
 }
