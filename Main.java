794bfda86e5cfb8cdb6bd868ad1a3902538a13aa
Hey0140 <lehy1124@soongsil.ac.kr>
2026-09-14 16:28:57 +0900

    [FEAT] DI + FACTORY

--- Main.java
+++ Main.java
@@ -16,9 +16,27 @@ class Node implements Comparable<Node>{
 
 
 public class Main {
+
+    static void A () {
+        CoffeeFactory coffeeFactory = new CoffeeFactory();
+        coffeeFactory.create("Latte", "l");
+    }
+
+    static void D (Coffee coffee) {
+        coffee.drank();
+        //히히 이렇게 드링켜~~
+    }
     public static void main(String[] args) {
         PriorityQueue<Node> pq = new PriorityQueue<>();
         // SuperCar car = new SuperCar("붕붕이");
         // car.ride();
+
+
+        CoffeeFactory coffeeFactory = new CoffeeFactory();
+        coffeeFactory.create("Americano", "m");
+    
+
+        D(coffeeFactory.create("Latte", "s"));
+        //이렇게 넘겨주어도 돼요 우히히
     }
 }
